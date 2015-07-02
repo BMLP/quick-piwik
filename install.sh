e83c1cfb1f077642cebe907cb6e2fafb31349bf6
Joe Grigg <account@joegrigg.com>
2015-07-02 09:50:02 +0100

    Finished install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "Install Starting..."
 mkdir piwik-mysql
 mkdir piwik-www
 
@@ -5,7 +6,12 @@ cd piwik-www
 wget http://builds.piwik.org/piwik.zip
 unzip piwik.zip
 rm How\ to\ install\ Piwik.html
-rm index.html
 rm piwik.zip
 mv piwik/* .
 cd ../
+
+docker-compose up -d
+docker-compose ps
+
+rm index.html
+echo "Install Complete..."
